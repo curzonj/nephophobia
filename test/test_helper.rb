fb1b36d637e9e0e1c5064d4dbc9d02a8bfdfb0cb
retr0h <john@dewey.ws>
2011-03-10 13:32:56 -0800

    + Moved path to cassettes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -31,7 +31,7 @@ end
 
 VCR.config do |c|
   c.stub_with :fakeweb
-  c.cassette_library_dir     = "test/fixtures/cassettes"
+  c.cassette_library_dir     = "test/cassettes"
   c.default_cassette_options = { :record => :none }
 end
 
